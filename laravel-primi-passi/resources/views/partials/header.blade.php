0430bdb926d0053880df5a11a7efc7feccdf403c
AgneseBettio <bettio.agnese@gmail.com>
2021-06-18 18:51:44 +0200

    navbar - impastata seguendo layout dato

--- laravel-primi-passi/resources/views/partials/header.blade.php
+++ laravel-primi-passi/resources/views/partials/header.blade.php
@@ -1,26 +1,73 @@
 <header>
 
 <nav>
-    <div class="logo-box">
+    {{-- <div class="logo-box">
         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/DC_Comics_logo.svg/1200px-DC_Comics_logo.svg.png" alt="DC logo">
-    </div>
-    <div class="blank-space">
-    </div>
+    </div> --}}
+    
     <div class="top-navbar">
-        <ul>
-            <li>
-                <a href="#">Home</a>
-            </li>
-            <li>
-                <a href="#">About</a>
-            </li>
-            <li>
-                <a href="#">News</a>
-            </li>
-            <li>
-                <a href="#">Shop</a>
-            </li>
-        </ul>
+        <div class="container">
+        
+            <div class="blank-space">
+            </div>
+            <div>
+                <ul>
+                    <li>
+                        DC POWER VISA@
+                    </li>
+                    <li>
+                        <a href="#">
+                            ADDITIONAL DC SITES <span><i class="fa fa-sort-desc" aria-hidden="true"></i></span>
+                        </a>
+                    </li>
+                </ul>
+
+            </div>
+
+        </div>
+       
+    </div>
+    <div class="main-navbar container">
+        <div class="logo-box">
+        </div>
+        <div class="links-box">
+                <ul>
+                    <li>
+                        <a href="#">CHARACTERS</a> 
+                    </li>
+                    <li>
+                        <a href="#">COMICS</a>                            
+                    </li>
+                    <li>
+                        <a href="#">MOVIES</a> 
+                    </li>
+                    <li>
+                        <a href="#">TV</a>                            
+                    </li>
+                    <li>
+                        <a href="#">GAMES</a> 
+                    </li>
+                    <li>
+                        <a href="#">COLLECTLIBLES</a>                            
+                    </li>
+                    <li>
+                        <a href="#">VIDEOS</a> 
+                    </li>
+                    <li>
+                        <a href="#">FANS</a>                            
+                    </li>
+                    <li>
+                        <a href="#">NEWS</a>                            
+                    </li>
+                    <li>
+                        <a href="#">
+                            SHOP <span><i class="fa fa-sort-desc" aria-hidden="true"></i></span>
+                        </a>                            
+                    </li>
+                </ul>
+
+            </div>
+
     </div>
 </nav>
 
